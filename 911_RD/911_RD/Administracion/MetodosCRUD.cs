dae8ded66f2cfb44319933c75cf29be8f4e81107
sociio1908 <57240808+sociio1908@users.noreply.github.com>
2021-07-14 21:59:41 -0400

    Update de BD y ENtities

--- 911_RD/911_RD/Administracion/MetodosCRUD.cs
+++ 911_RD/911_RD/Administracion/MetodosCRUD.cs
@@ -10,47 +10,41 @@ namespace _911_RD.Administracion
     {
         public int crudTerceros(string id_traido, string nombre_traido, string identificacion_traida)
         {
-
-           int id_result = 0;
-            try {
-                using (TransporSysEntities db = new TransporSysEntities())
-                {
-                    var terceroquery = db.TERCEROS.FirstOrDefault(a => a.identificacion.ToString() == identificacion_traida.Trim());
-                //    System.Diagnostics.Debug.WriteLine(terceroquery.nombre);
-                    if (terceroquery == null) { 
-                        TERCEROS tercero = new TERCEROS
-                        {
-                            nombre = nombre_traido.Trim(),
-                            identificacion = identificacion_traida.Trim(),
-                         };
-                        db.TERCEROS.Add(tercero);
-                    }
-                    else
-                    {
-                      terceroquery.nombre = nombre_traido.Trim();
-                      terceroquery.identificacion = identificacion_traida.Trim();
-                    }
-                    db.SaveChanges();
-                    if (terceroquery == null) { 
-                        id_result = db.TERCEROS.Max(x => x.id_tercero);
-                    }
-                    else
-                    {
-                        id_result = terceroquery.id_tercero;
-                    }
-                    System.Diagnostics.Debug.WriteLine("Proceso exitoso.");
-                }
-             }
-            catch(Exception aas)
+            try
             {
-                System.Diagnostics.Debug.WriteLine("Hubo un error chulo. "+aas);
+                using(TransporSysEntities bd = new TransporSysEntities())
+                {
+
+
+                    if (identificacion_traida == "0" || identificacion_traida == null)
+                    {
+
+
+
+                    }
+
+
+
+
+
+
+                }
+
+
+            }
+            catch(Exception asd)
+            {
+
             }
 
-            return id_result;
+
+
+
+
+
+            return 0;
         }
 
-
-    
 
         public int crudPersonas(string id_per_traido, string id_ter_traido, string id_sex_traido, DateTime fecha_nac ,string id_nac_traido, string apellido_traido)
         {
